613c0f038832d0881b317402ae84f6b0fcc4cd02
PenguinPops <msoltys.biz@gmail.com>
2024-10-09 08:41:03 +0200

    C-3-091024 lab2 swift

---
+++ swiftlab/lab2/zad1.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let pi = 3.14
+var a = 4, str = "abc", c = 5.4329
+    
+print(pi, str)
+print("Pi: \(pi), str: \(str)")
+print("Pi: ",pi,", str: ",str)
+
+var str2 = String(format: "%.2f", c)
+print(str2)
+
